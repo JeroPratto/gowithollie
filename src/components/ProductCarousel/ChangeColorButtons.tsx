302df2ca11738009368b63b7c802d17a9d9500e4
Jeronimo Pratto <jeropratto@yahoo.com.ar>
2024-09-09 11:45:22 -0300

    improve accessibility

--- src/components/ProductCarousel/ChangeColorButtons.tsx
+++ src/components/ProductCarousel/ChangeColorButtons.tsx
@@ -5,14 +5,22 @@ export default function ChangeColorButtons() {
 	const { color, setColor } = useColorStore()
 	return (
 		<div className={styles.changeColor}>
-			<button className={styles.blackBtn} onClick={() => setColor('Black')}>
+			<button
+				className={styles.blackBtn}
+				onClick={() => setColor('Black')}
+				aria-label='select black color'
+			>
 				<span
 					className={`${styles.indicator} ${
 						color === 'Black' ? styles.current : ''
 					}`}
 				></span>
 			</button>
-			<button className={styles.greenBtn} onClick={() => setColor('Green')}>
+			<button
+				className={styles.greenBtn}
+				onClick={() => setColor('Green')}
+				aria-label='select green color'
+			>
 				<span
 					className={`${styles.indicator}  ${
 						color === 'Green' ? styles.current : ''
