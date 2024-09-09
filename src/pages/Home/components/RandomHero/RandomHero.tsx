4e628cfb22223b2a00cdd80875c39c35fe48df7b
Jeronimo Pratto <jeropratto@yahoo.com.ar>
2024-09-09 11:21:57 -0300

    add skeleton

--- src/pages/Home/components/RandomHero/RandomHero.tsx
+++ src/pages/Home/components/RandomHero/RandomHero.tsx
@@ -5,10 +5,19 @@ import ollieLogo from '/hero/ollie.svg'
 
 export default function RandomHero() {
 	const [{ urlImage, text: title }] = useState(getRandomHero())
+	const [isLoading, setIsLoading] = useState(true)
+	const handleImageLoad = () => setIsLoading(false)
+
 	return (
 		<section className={styles.section}>
 			<h1 className={styles.title}>{title}</h1>
-			<img src={urlImage} alt='' className={styles.image} />
+			{isLoading && <div className={styles.skeleton}>test</div>}
+			<img
+				src={urlImage}
+				alt=''
+				className={`${styles.image} ${isLoading ? styles.hidden : ''}`}
+				onLoad={handleImageLoad}
+			/>
 			<img src={ollieLogo} alt='' className={styles.ollieLogo} />
 		</section>
 	)
